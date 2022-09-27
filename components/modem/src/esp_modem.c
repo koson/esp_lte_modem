60eb1227fed2048fe93331bdae73193a7a160cc2
Koson Trachu <koson.trachu@gmail.com>
2022-09-28 00:34:51 +0700

    Update esp_modem.c

--- components/modem/src/esp_modem.c
+++ components/modem/src/esp_modem.c
@@ -405,6 +405,7 @@ static esp_err_t esp_modem_dte_send_cmd(modem_dte_t *dte, const char *command, u
 {
     esp_err_t ret = ESP_FAIL;
     modem_dce_t *dce = dte->dce;
+    ESP_LOGD(MODEM_TAG, "Sending command:%s", command);
     MODEM_CHECK(dce, "DTE has not yet bind with DCE", err);
     MODEM_CHECK(command, "command is NULL", err);
     esp_modem_dte_t *esp_dte = __containerof(dte, esp_modem_dte_t, parent);
